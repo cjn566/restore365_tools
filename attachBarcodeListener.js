5e38920a4dd1e89e5676a878bc99c0a97fb3a765
cjn566 <cjn566@gmail.com>
2018-12-10 17:53:12 -0800

    Update attachBarcodeListener.js

--- attachBarcodeListener.js
+++ attachBarcodeListener.js
@@ -1,11 +1,30 @@
-showMenuButton = document.querySelector("#ctl00_ContentPlaceHolder1_gvEquipment_ctl00_ctl02_ctl03_Filter_BarCodeText");
-btn_thing = document.querySelector("#ctl00_ContentPlaceHolder1_gvEquipment_rfltMenu_detached").childNodes[1].childNodes[2].childNodes[0];
+(function() {
 
-onKey = function(e){
-    if(e.keyCode == "13"){
-        showMenuButton.click();
-        btn_thing.click();
-    }
-};
+    (attachKeyListener = ()=>{
+        showMenuButton = document.querySelector("#ctl00_ContentPlaceHolder1_gvEquipment_ctl00_ctl02_ctl03_Filter_BarCodeText");
+            btn_thing = document.querySelector("#ctl00_ContentPlaceHolder1_gvEquipment_rfltMenu_detached").childNodes[1].childNodes[2].childNodes[0];
 
-document.querySelector("#ctl00_ContentPlaceHolder1_gvEquipment_ctl00_ctl02_ctl03_FilterTextBox_BarCodeText").addEventListener("keypress",onKey);
+            onKey = function(e){
+                if(e.keyCode == "13"){
+                    showMenuButton.click();
+                    btn_thing.click();
+                }
+            };
+
+            document.querySelector("#ctl00_ContentPlaceHolder1_gvEquipment_ctl00_ctl02_ctl03_FilterTextBox_BarCodeText").addEventListener("keypress",onKey);
+    })();
+
+
+    var origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        console.log('request started!');
+        this.addEventListener('load', function() {
+            console.log('request completed!');
+
+            attachKeyListener();
+
+            console.log(this.readyState); //will always be 4 (ajax is completed successfully)
+        });
+        origOpen.apply(this, arguments);
+    };
+})();
